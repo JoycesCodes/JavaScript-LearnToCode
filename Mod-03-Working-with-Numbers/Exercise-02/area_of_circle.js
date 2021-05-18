5f23900e3c81ea8d10819c6969d229fb8a585069
Joyce Young <jjyoung145@gmail.com>
2021-05-18 15:00:49 -0400

    changed exercise 4 using math object

--- Mod-03-Working-with-Numbers/Exercise-02/area_of_circle.js
+++ Mod-03-Working-with-Numbers/Exercise-02/area_of_circle.js
@@ -1,7 +1,7 @@
 "use script"
 
 let radius = 2;
-let pi = 3.1415;
-let area = pi* 2**2;
+//let pi = 3.1415;
+let area = Math.PI* 2**2;
 
-console.log("The area of a circle with radius " + radius + " is " + area);
+console.log("The area of a circle with radius " + radius + " is " + area.toFixed(4));
